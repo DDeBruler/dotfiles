b96dd96b1d64ce5245c22ebccd5754b54a78ec33
Dan DeBruler <ddebruler@gmail.com>
2020-10-30 09:53:29 -0400

    Install 1Password in Ubuntu setup

--- environments/ubuntu.sh
+++ environments/ubuntu.sh
@@ -75,6 +75,13 @@ if ! test e /usr/pgadmin4; then
   sudo apt install -y pgadmin4-desktop
 fi
 
+# 1Password
+if ! command -v 1password &> /dev/null; then
+  sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+  echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://downloads.1password.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
+  sudo apt update && sudo apt install 1password
+fi
+
 ### CONFIGURE ENVIRONMENT
 
 # postgres
